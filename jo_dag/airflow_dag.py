7bd5c6df89f9d5e5164f63caaa34524f3881a025
hamza <hamza.hord@gmail.com>
2024-07-20 17:23:43 +0200

    push dag with cloud storage

--- jo_dag/airflow_dag.py
+++ jo_dag/airflow_dag.py
@@ -4,8 +4,10 @@ from airflow.hooks.base_hook import BaseHook
 from datetime import datetime, timedelta
 import pandas as pd
 from supabase_py import create_client, Client
+from azure.storage.blob import BlobServiceClient
 import os
 import json
+import io
 
 # Configurer les arguments par défaut pour le DAG
 default_args = {
@@ -16,24 +18,48 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-# Chemin vers le fichier data.txt
-file_path = '/home/hamza/jo.csv'
+# Configuration Azure Blob Storage
+azure_blob_conn_id = 'azure_blob_conn_id'
+container_name = 'datalakeblob'
+
+# Fonction pour récupérer les fichiers CSV depuis Azure Blob Storage
+def get_blob_service_client():
+    connection_string = BaseHook.get_connection(azure_blob_conn_id).extra_dejson.get('connection_string')
+    return BlobServiceClient.from_connection_string(connection_string)
+
+def get_csv_files_from_blob():
+    blob_service_client = get_blob_service_client()
+    container_client = blob_service_client.get_container_client(container_name)
+    blobs = container_client.list_blobs(name_starts_with="jo")
+    csv_files = [blob.name for blob in blobs if blob.name.endswith('.csv')]
+    return csv_files
+
+def read_csv_from_blob(blob_name):
+    blob_service_client = get_blob_service_client()
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+    stream = io.BytesIO()
+    blob_client.download_blob().readinto(stream)
+    stream.seek(0)
+    return pd.read_csv(stream, delimiter=';')
 
 # Fonction pour traiter les données
 def process_data(**kwargs):
-    # Lire le fichier CSV
-    data = pd.read_csv(file_path, delimiter=';')
+    # Lire tous les fichiers CSV correspondant
+    csv_files = get_csv_files_from_blob()
+    combined_data = pd.DataFrame()
+    for csv_file in csv_files:
+        data = read_csv_from_blob(csv_file)
+        combined_data = pd.concat([combined_data, data], ignore_index=True)
 
     # Split the rows based on the sports column
     split_rows = []
 
-    for index, row in data.iterrows():
+    for index, row in combined_data.iterrows():
         sports = row['Sports'].split(',')
         for sport in sports:
             new_row = row.copy()
             new_row['Sports'] = sport.strip()
             split_rows.append(new_row)
-
 
     # Créer une DataFrame à partir des lignes éclatées
     processed_data = pd.DataFrame(split_rows)
@@ -49,6 +75,7 @@ def process_data(**kwargs):
     processed_data['adress'] = processed_data['adress'].astype(str)
 
     df_json = processed_data.to_json(orient='records', date_format='iso')
+
     # Pousser les données prétraitées dans XCom
     kwargs['ti'].xcom_push(key='processed_data', value=df_json)
     print("success")
